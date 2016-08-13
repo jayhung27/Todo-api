07fbc3bd07147a363e1d7e864e908cafd5fdd1d2
Jay Hung <jayhung27@gmail.com>
2016-08-13 18:10:06 +0800

    Add ?completed filter And Add variable-playground.js

---
+++ variable-playground.js
@@ -0,0 +1,11 @@
+var grades = [87, 90];
+
+function addGrades (grades){
+    grades = [1, 2, 3];
+    debugger;
+    console.log(grades);
+    //return grades;
+}
+
+grades = addGrades(grades);
+console.log(grades);
